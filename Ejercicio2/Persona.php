c79e84c6c0422210952560cd793a0c675da8859b
Leonardo <leonardo38361@gmail.com>
2023-10-09 03:20:31 -0300

    extiende de Familia, mejora de metodo imprimir

--- Ejercicio2/Persona.php
+++ Ejercicio2/Persona.php
@@ -1,24 +1,14 @@
 <?php
-class Persona
-{
-    private string $nombre;
+require_once 'Familia.php'; // Asegúrate de incluir el archivo que contiene Casados
+
+class Persona extends Familia
+{ 
     private string $nacimiento;
-    private $familia = array();
-    private $conyuge = array();
 
     public function __construct(string $nombre)
     {
-        $this->nombre = $nombre;
+        parent::__construct($nombre);
         $this->nacimiento = date("d/m/y");
-    }
-
-
-    /**
-     * @return string
-     */
-    public function getNombre(): string
-    {
-        return $this->nombre;
     }
 
     /**
@@ -31,7 +21,6 @@ class Persona
         return $this;
     }
 
-
     public function contarEdad(): int
     {
         $fechaNacimiento = date($this->nacimiento);
@@ -42,64 +31,32 @@ class Persona
         return $edad;
     }
 
-    public function esMayorDeEdad(): bool
-    {
-        $edad = $this->contarEdad();
-        return $edad >= 18;
-    }
-
-    public function presentarFamilia(): array
-    {
-        return $this->familia;
-    }
-
-    public function agregarFamiliar(Persona $familiar)
-    {
-        $this->familia[] = $familiar;
-    }
-
-    public function agregarConyuge(Persona $conyuge)
-    {
-        $esMayor = $this->esMayorDeEdad();
-        $esMayorConyuge = $conyuge->esMayorDeEdad();
-        if ($esMayor && $esMayorConyuge && count($this->conyuge) < 1) {
-            $tieneConyuge = false;
-            foreach ($this->conyuge as $c) {
-                if ($c->nombre === $conyuge->nombre) {
-                    $tieneConyuge = true;
-                    break;
-                }
-            }
-            if (!$tieneConyuge) {
-                array_unshift($this->conyuge, $conyuge);
-            }
-        }
-    }
-
     public function imprimir()
     {
         echo "Nombre: " . $this->nombre . "<br>";
         echo "Fecha de Nacimiento: " . $this->nacimiento . "<br>";
         echo "Edad: " . $this->contarEdad() . " años<br>";
 
-        if ($this->esMayorDeEdad()) {
-            echo "Es mayor de edad.<br>";
-        } else {
-            echo "No es mayor de edad.<br>";
-        }
+        echo "Familiares de sangre : ";
 
-        echo "Familiares:<br>";
+
+        $count = count($this->familia);
+        $index = 0;
+        
         foreach ($this->familia as $familiar) {
             if ($familiar instanceof Persona) {
-                echo $familiar->nombre . "<br>";
+                echo $familiar->nombre;
+                if($index < $count - 2 ){
+                    echo " * ";
+                }
+                $index++;
             }
         }
-
-        echo "Cónyuge:<br>";
-        foreach ($this->conyuge as $conyuge) {
-            if ($conyuge instanceof Persona) {
-                echo $conyuge->nombre . "<br>";
+        echo "<br>";
+        echo "Cónyuge: ";
+            if ($this->conyuge instanceof Conyuge) {
+                echo $this->conyuge->getNombre() . "";
             }
         }
     }
-}
+
