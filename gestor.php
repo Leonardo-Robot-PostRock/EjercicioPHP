3ca539818bc64d97c6f16b4349003183b1ab46e5
Leonardo <leonardo38361@gmail.com>
2023-10-10 13:03:33 -0300

    se crea conexion a base de datos con la clase PDO de php

--- gestor.php
+++ gestor.php
@@ -7,27 +7,37 @@ class GestorBD
         string $user,
         string $password
     ) {
-        $this->c = new PDO(
-            "mysql:host=localhost;
-            dbname = $dbname;
+        try {
+
+            $this->c = new PDO(
+                "mysql:host=localhost;
+            dbname = {$dbname};
             charset=utf8m64",
-            $user,
-            $password,
-            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-        );
+                $user,
+                $password,
+                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+            );
+        } catch (PDOException $e) {
+            throw new Exception("Error al conectar a la base de datos: " . $e->getMessage());
+        }
     }
 
     public function leer(string $tabla, array $criterio): array
     {
-        $sql = "SELECT * FROM  $tabla";
-        if (!empty($criterio)) {
-            $sql .= ' WHERE ';
-            $sql = implode(' AND ', array_map(fn($k) => $k . '= :' . $k, array_keys($criterio)));
+        try {
+
+            $sql = "SELECT * FROM  $tabla";
+            $whereClause = [];
+            if (!empty($criterio)) {
+                $sql .= ' WHERE ';
+                $sql = implode(' AND ', array_map(fn ($k) => $k . '= :' . $k, array_keys($criterio)));
+            }
+            $s = $this->c->prepare($sql);
+            $s->execute($criterio);
+            $r = $s->fetchAll(PDO::FETCH_ASSOC);
+            return $r;
+        } catch (PDOException $e) {
+            throw new Exception("Error al leer de la base de datos: " . $e->getMessage());
         }
-        $s = $this->c->prepare($sql);
-        $s->execute($criterio);
-        $r = $s->fetchAll(PDO::FETCH_ASSOC);
-        return $r;
     }
 }
-?>
