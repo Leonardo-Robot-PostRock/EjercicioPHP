5b029bc6fae75eab5dae63d12fa6148c728d9cd6
Leonardo <leonardo38361@gmail.com>
2023-10-10 13:33:20 -0300

    mejora del metodo read de php

--- gestor.php
+++ gestor.php
@@ -22,20 +22,23 @@ class GestorBD
         }
     }
 
-    public function leer(string $tabla, array $criterio): array
+    public function read(string $tabla, array $criterio): array
     {
         try {
 
             $sql = "SELECT * FROM  $tabla";
             $whereClause = [];
-            if (!empty($criterio)) {
-                $sql .= ' WHERE ';
-                $sql = implode(' AND ', array_map(fn ($k) => $k . '= :' . $k, array_keys($criterio)));
+            foreach ($criterio as $key => $value) {
+                $whereClause[] = "{key} = :{$key}";
             }
-            $s = $this->c->prepare($sql);
-            $s->execute($criterio);
-            $r = $s->fetchAll(PDO::FETCH_ASSOC);
-            return $r;
+            if (!empty($whereClause)) {
+                $sql .= ' WHERE ';
+                $sql = " WHERE " . implode(" AND ", $whereClause);
+            }
+            $stmt = $this->c->prepare($sql);
+            $stmt->execute($criterio);
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
         } catch (PDOException $e) {
             throw new Exception("Error al leer de la base de datos: " . $e->getMessage());
         }
