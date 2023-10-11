0e84de0de318896902437a573f44721a33b2b2cf
Leonardo <leonardo38361@gmail.com>
2023-10-11 00:06:35 -0300

    comentarios sobre el ejercicio

--- Ejercicio5.php
+++ Ejercicio5.php
@@ -26,3 +26,12 @@ class Router
         if (!$match) http_response_code(404);
     }
 }
+// Configuración de Rutas y Métodos HTTP: La clase Router debería tener propiedades para almacenar las rutas y métodos HTTP que se utilizarán para enrutar solicitudes. Estas rutas pueden definirse con patrones y asociarse con controladores o acciones específicas.
+
+// Método route(): El método route() es el punto de entrada principal del enrutador. Cuando se llama a este método, se intenta determinar la ruta actual de la solicitud y se busca una coincidencia en las rutas definidas. Si se encuentra una coincidencia, se verifica si el método HTTP de la solicitud coincide con el método HTTP definido en la ruta.
+
+// Parseo de la Solicitud: En el fragmento de código proporcionado, se parsea el cuerpo de la solicitud en caso de que el método HTTP no sea GET ni POST. Esto se hace para manejar datos de solicitud en otros métodos HTTP, como PUT o DELETE.
+
+// Ejecución de Acciones: Si se encuentra una coincidencia de ruta y método HTTP, el código itera a través de las acciones asociadas a esa ruta y las ejecuta. Cada acción puede ser una función o un método que realiza alguna lógica específica en respuesta a la solicitud.
+
+// Código de Respuesta 404: Si no se encuentra ninguna coincidencia en las rutas definidas, se establece el código de respuesta HTTP en 404, lo que indica que la ruta no ha sido encontrada.
