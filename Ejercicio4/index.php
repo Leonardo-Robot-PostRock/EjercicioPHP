98c1c1c887fa5f7e314b375c06ff154f5987b4df
Leonardo <leonardo38361@gmail.com>
2023-10-10 23:01:33 -0300

    iniciar session

---
+++ Ejercicio4/index.php
@@ -0,0 +1,19 @@
+<?php
+session_start(); // Iniciar la sesión si no está iniciada
+
+if (isset($_SESSION['usuario'])) {
+    // El usuario ya ha iniciado sesión
+    echo "Bienvenido, " . $_SESSION['usuario'] . "!<br>";
+    echo "<a href='cerrar_sesion.php'>Cerrar Sesión</a>";
+} else {
+    // El usuario no ha iniciado sesión
+    echo "Por favor, inicie sesión:<br>";
+    echo "<form method='post' action='login.php'>";
+    $usuario = isset($_COOKIE['usuario']) ? $_COOKIE['usuario'] : '';
+    echo "Usuario: <input type='text' name='usuario' value='$usuario'><br>";
+    echo "Contraseña: <input type='password' name='contrasena'><br>";
+    echo "<input type='submit' value='Iniciar Sesión'>";
+    echo "</form>";
+}
+echo "<br>cookies:<br>";
+print_r($_COOKIE);
